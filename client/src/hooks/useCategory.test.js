5a20f25dcf591cfa3587b97acef09e31e19154be
aureliony <39163684+aureliony@users.noreply.github.com>
2026-02-09 05:31:38 +0800

    Add unit tests for useCategory hook

---
+++ client/src/hooks/useCategory.test.js
@@ -0,0 +1,54 @@
+import { renderHook, waitFor } from "@testing-library/react";
+import axios from "axios";
+import useCategory from "./useCategory";
+import testCategories from "../../../data/test.categories.json";
+
+jest.mock("axios");
+
+describe("useCategory Hook", () => {
+  it("should not throw errors when api returns empty data", async () => {
+    axios.get.mockResolvedValueOnce({ data: { category: [] } });
+
+    const { result } = renderHook(() => useCategory());
+
+    await waitFor(() => {
+      expect(result.current).toEqual([]);
+    });
+
+    expect(axios.get).toHaveBeenCalled();
+  });
+
+  it("should not throw errors when api returns invalid data", async () => {
+    axios.get.mockResolvedValueOnce({ data: { category: 420 } });
+
+    const { result } = renderHook(() => useCategory());
+
+    await waitFor(() => {
+      expect(result.current).toEqual([]);
+    });
+
+    expect(axios.get).toHaveBeenCalled();
+  });
+
+  it("should fetch categories and update state correctly", async () => {
+    axios.get.mockResolvedValueOnce({ data: { category: testCategories } });
+
+    const { result } = renderHook(() => useCategory());
+
+    await waitFor(() => {
+      expect(result.current).toEqual(testCategories);
+    });
+
+    expect(axios.get).toHaveBeenCalled();
+  });
+
+  it("should handle api/network errors gracefully", async () => {
+    axios.get.mockRejectedValue(new Error("Unit Test"));
+
+    const { result } = renderHook(() => useCategory());
+
+    await waitFor(() => {
+      expect(result.current).toEqual([]);
+    });
+  });
+});
--- jest.frontend.config.js
+++ jest.frontend.config.js
@@ -20,11 +20,17 @@ export default {
   transformIgnorePatterns: ["/node_modules/(?!(styleMock\\.js)$)"],
 
   // only run these tests
-  testMatch: ["<rootDir>/client/src/pages/Auth/*.test.js"],
+  testMatch: [
+    "<rootDir>/client/src/hooks/*.test.js",
+    "<rootDir>/client/src/pages/Auth/*.test.js"
+  ],
 
   // jest code coverage
   collectCoverage: true,
-  collectCoverageFrom: ["client/src/pages/Auth/**"],
+  collectCoverageFrom: [
+    "client/src/hooks/**",
+    "client/src/pages/Auth/**"
+  ],
   coverageThreshold: {
     global: {
       lines: 100,
